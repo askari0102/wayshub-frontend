b558e86af17a28b6d4a829d8981c7d9b4c1f7f46
askari0102 <muwahidunaskari@gmail.com>
2026-04-12 18:03:23 +0700

    test cicd

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
 import { render } from '@testing-library/react';
 import App from './App';
+import { AppContextProvider } from './context/AppContext'; // Sesuaikan path ini dengan letak AppContext kamu
 
 test('renders app without crashing', () => {
-  render(<App />);
+  render(
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  );
 });
