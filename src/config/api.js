06cd3547297553902793bc917d702d6afa9a555d
askari0102 <muwahidunaskari@gmail.com>
2026-04-13 07:25:36 +0700

    change to https

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "http://api.kelompok2.studentdumbways.my.id/api/v1"
+    baseURL: "https://api.kelompok2.studentdumbways.my.id/api/v1"
 });
 
 const setAuthToken = (token) => {
